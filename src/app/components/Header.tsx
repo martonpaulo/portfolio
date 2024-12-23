606dd7faf858ba68553e76f8a36104ae211b60c8
Marton Paulo <martonpss@gmail.com>
2024-12-23 14:19:32 -0300

    fix: corrected SVG rendering size and font size for responsiveness

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,15 +7,28 @@ import Cover from "@/app/components/Cover";
 export default function Header() {
   const { resolvedTheme } = useTheme();
   const [themeMode, setThemeMode] = useState("light");
+  const [size, setSize] = useState(150);
 
   useEffect(() => {
     setThemeMode(resolvedTheme === "dark" ? "dark" : "light");
   }, [resolvedTheme]);
 
+  useEffect(() => {
+    const handleResize = () => {
+      const newSize = Math.min(window.innerWidth / 5.5, 150);
+      setSize(newSize);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Set initial size
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <header className="flex flex-col items-center mb-8">
-      <Cover mode={themeMode} size={150} />
-      <p className="text-xl sm:text-2xl font-mono mt-2">
+      <Cover mode={themeMode} size={size} />
+      <p className="text-l sm:text-2xl font-mono text-center">
         React Front-End Developer
       </p>
     </header>
--- src/app/components/UnderConstruction.tsx
+++ src/app/components/UnderConstruction.tsx
@@ -3,7 +3,7 @@ import { Construction } from "lucide-react";
 export default function UnderConstruction() {
   return (
     <section className="mt-4 pb-16 flex flex-col items-center text-center">
-      <Construction className="w-24 h-24 mt-4" />
+      <Construction className="w-16 sm:w-24 h-16 sm:h-24 mt-4" />
       <h2 className="text-xl sm:text-2xl font-bold mt-4">
         This Page is Under Construction!
       </h2>
